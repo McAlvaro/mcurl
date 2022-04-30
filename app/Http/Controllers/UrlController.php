43af3a51b127c4b3ed6ab366d0c2663d5832d76b
McAlvaro <mc.alv641@gmail.com>
2022-04-30 02:47:53 -0400

    Added Logic Backend for create short url

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Url;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UrlController extends Controller
 {
-    //
+
+    public function show( Request $request, $code ){
+
+        $url = DB::table('urls')->where('code', $code)->first();
+
+        if( $url ){
+
+            return redirect()->away($url->url);
+
+        } else {
+
+            abort(404);
+
+        }
+
+    }
+
+    public function store ( Request $request, Url $url ){
+
+        $url = $url->short_url( $request->long_url );
+
+        return response()->json([
+            'short_url' => env('APP_URL') . '/' . $url
+        ]);
+
+    }
+
 }
--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\CodeGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,8 +10,31 @@ class Url extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['url', 'user_id'];
+
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    /* SHORT URL */
+
+    public function short_url( $long_url ){
+
+        // Create URL
+        $url = self::create([
+            'url' => $long_url,
+            'user_id' => auth()->user()->id
+        ]);
+
+        // Generate Code
+        $code = ( new CodeGenerator() )->get_code($url->id);
+
+        // Update URL
+        $url->code = $code;
+        $url->save();
+
+        return $url->code;
+
     }
 
 }
